7639bf96644d76eaa898b46c36993d0829787fcf
dfidelity <141320589+dfidelity@users.noreply.github.com>
2023-09-18 20:05:01 +0530

    Create Config.cs

---
+++ PPSLambda/Config/Config.cs
@@ -0,0 +1,14 @@
+namespace PPSLambda.Config
+{
+    public class Config
+    {
+        public string SplunkKey { get; set; }
+
+        public string SplunkUrl { get; set; }
+        public string S3BucketName { get; set; }
+        public string RingBoxURL { get; set; }
+        public string RoyalBoxURL { get; set; }
+        public string FeatureToggles { get; set; }
+
+    }
+}
