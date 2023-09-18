019059ce86ab52ffa0c507e85bfb2aab4e1998e5
dfidelity <141320589+dfidelity@users.noreply.github.com>
2023-09-18 15:35:24 +0100

    Add files via upload

---
+++ PPSLambda/Config/ConfigFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace PPSLambda.Config
+{
+    public static class ConfigFactory
+    {
+        public static Config GetConfig()
+        {
+            using (var r = File.OpenText(@"./config.json"))
+            {
+                var json = r.ReadToEnd();
+                return JsonConvert.DeserializeObject<Config>(json);
+            }
+        }
+    }
+}
